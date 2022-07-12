fb0a6ce57c4a85581174baeb99cb75aa888ff03a
Alexey Rodionov <rodionlekseev@gmail.com>
2022-07-12 08:07:30 +0300

    Add OrdersController

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,51 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to @order
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+
+    if @order.destroy
+      redirect_to orders_path
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customers_name)
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
   has_many :services, dependent: :destroy
+  validates :customers_name, presence: true
 end
